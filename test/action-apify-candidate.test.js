b9895741d15d015c768d8afd0f8906eced7966f4
coachhornsby <coachhornsby@gmail.com>
2026-09-21 11:15:47 -0500

    chore: advance production schema contract to 0029

--- test/action-apify-candidate.test.js
+++ test/action-apify-candidate.test.js
@@ -431,7 +431,7 @@ test("migration 0021/0022 + schema.extensions + health expected migration", asyn
     assert.match(schema, new RegExp(t));
   }
   assert.match(schema, /shadow_candidate_scheduler_state/);
-  assert.match(health, /EXPECTED_MIGRATION\s*=\s*["']0028_executed_bet_tracker_metadata["']/);
+  assert.match(health, /EXPECTED_MIGRATION\s*=\s*["']0029_executed_bet_settlement_evidence["']/);
   assert.match(health, /actionApifyCandidateHealth/);
   assert.match(health, /actionApify:/);
 });
